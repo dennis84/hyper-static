c52f6597eeb83248cf9f866aba1cdb9fe33e284e
Dennis Dietrich <d.dietrich84@gmail.com>
2018-04-11 14:33:27 +0200

    hyper master

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-extern crate mime;
 extern crate futures;
 extern crate hyper;
 extern crate flate2;
@@ -11,30 +10,29 @@ use std::io::{Read, Write};
 use futures::future;
 use futures::future::FutureResult;
 
-use hyper::server::{Request, Response};
-use hyper::header;
-use hyper::StatusCode;
+use hyper::{Request, Response, StatusCode, Body};
 
 use flate2::Compression;
 use flate2::write::GzEncoder;
 
-fn get_content_type(path: &Path) -> header::ContentType {
+fn get_content_type(path: &Path) -> &str {
     match path.extension().and_then(OsStr::to_str) {
-        Some("html") => header::ContentType(mime::TEXT_HTML),
-        Some("css")  => header::ContentType(mime::TEXT_CSS),
-        Some("js")   => header::ContentType(mime::TEXT_JAVASCRIPT),
-        _            => header::ContentType(mime::TEXT_PLAIN),
+        Some("html") => "text/html",
+        Some("css")  => "text/css",
+        Some("js")   => "text/javascript",
+        _            => "text/plain",
     }
 }
 
-pub fn from_dir(base_path: &str, req: Request)
-        -> FutureResult<Response, hyper::Error> {
-    let file_path = format!("{}{}", base_path, req.path());
+pub fn from_dir(base_path: &str, req: Request<Body>)
+        -> FutureResult<Response<Body>, hyper::Error> {
+    let file_path = format!("{}{}", base_path, req.uri().path());
     let file_path = Path::new(&file_path);
     if !file_path.is_file() {
-        return future::ok(Response::new()
-            .with_status(StatusCode::NotFound)
-            .with_body("Not Found"));
+        return future::ok(Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body("Not Found".into())
+            .unwrap());
     }
 
     let mut file = File::open(file_path).unwrap();
@@ -45,10 +43,9 @@ pub fn from_dir(base_path: &str, req: Request)
     encoder.write_all(body.as_slice()).unwrap();
     let compressed_bytes = encoder.finish().unwrap();
 
-    future::ok(Response::new()
-        .with_header(get_content_type(file_path))
-        .with_header(header::ContentEncoding(vec![
-            header::Encoding::Gzip
-        ]))
-        .with_body(compressed_bytes))
+    future::ok(Response::builder()
+        .header("Content-Type", get_content_type(file_path))
+        .header("Content-Encoding", "gzip")
+        .body(compressed_bytes.into())
+        .unwrap())
 }
