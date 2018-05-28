420d311e9a4acc914903c48fa3a0e9b5f3beb045
Dennis Dietrich <d.dietrich84@gmail.com>
2018-05-28 15:02:13 +0200

    use hyper 0.11

--- src/lib.rs
+++ src/lib.rs
@@ -29,10 +29,9 @@ pub fn from_dir(base_path: &str, req: Request<Body>)
     let file_path = format!("{}{}", base_path, req.uri().path());
     let file_path = Path::new(&file_path);
     if !file_path.is_file() {
-        return future::ok(Response::builder()
-            .status(StatusCode::NOT_FOUND)
-            .body("Not Found".into())
-            .unwrap());
+        return future::ok(Response::new()
+            .with_status(StatusCode::NotFound)
+            .with_body("Not Found"));
     }
 
     let mut file = File::open(file_path).unwrap();
@@ -43,9 +42,9 @@ pub fn from_dir(base_path: &str, req: Request<Body>)
     encoder.write_all(body.as_slice()).unwrap();
     let compressed_bytes = encoder.finish().unwrap();
 
-    future::ok(Response::builder()
-        .header("Content-Type", get_content_type(file_path))
-        .header("Content-Encoding", "gzip")
-        .body(compressed_bytes.into())
-        .unwrap())
+    let mut response = Response::new();
+    response.headers_mut().set_raw("Content-Type", get_content_type(file_path));
+    response.headers_mut().set_raw("Content-Encoding", "gzip");
+    response.set_body(compressed_bytes);
+    future::ok(response)
 }
